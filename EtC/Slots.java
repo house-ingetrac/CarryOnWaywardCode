c2f9979e7e8e98603d8907f2dbb3bf142777806d
Ricky Chen <rchen10@stuy.edu>
2017-01-12 10:16:01 -0500

    cs1 and Slots

--- EtC/Slots.java
+++ EtC/Slots.java
@@ -1,3 +1,26 @@
-public class Slots{
-    
-]
+public class Slots extends Minigames{
+    private String[][] machine = { {"7", "cherry", "pear", "zebra", "unicorn", "piccolo"},
+				   {"7", "cherry", "pear", "zebra", "unicorn", "piccolo"},
+				   {"7", "cherry", "pear", "zebra", "unicorn", "piccolo"}, };
+
+    public Slots(){
+	betAmount = 0;
+	multiplier = 1;//to be Driver.getMoney() / constant
+    }
+
+    public void play(){
+	
+    }
+
+    private void pull(){
+
+    }
+
+    private void RPGBattle(){
+
+    }
+
+    private void eatBurger(){
+
+    }
+}
