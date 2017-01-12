d6cf8e110c0476f2e00ad6a6beecd923b7ef2cfa
Ricky Chen <rchen10@stuy.edu>
2017-01-12 09:56:20 -0500

    Stuffs

---
+++ EtC/Minigames.java
@@ -0,0 +1,9 @@
+public abstract Minigames{
+    protected double multiplier;
+    protected int betAmount;
+
+    protected abstract void RPGBattle();
+    protected abstract void eatBurger();
+    protected abstract void bet();
+    
+}
