1cb776b8179d9858fc7654525e2e2e85a8e3d2b5
Alessandro Cartegni <acartegni@gmail.com>
2017-01-12 20:02:24 -0500

    update

---
+++ EtC/Roulette.java
@@ -0,0 +1,25 @@
+import cs1.Keyboard;
+
+public class Roulette extends Minigames{
+
+    public Roulette(){
+	betAmount = 0;
+	multiplier = 1;//to be Driver.getMoney() / constant
+    }
+
+    public void play(){
+	
+    }
+
+    private void spin(){
+	int result = (int)Math.random() * 41;
+    }
+
+    private void RPGBattle(){
+	
+    }
+
+    private void eatBurger(){
+	
+    }
+}
