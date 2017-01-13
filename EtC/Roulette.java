53d98e1995ee0fd73beb890731d1670b86a55db9
Alessandro Cartegni <acartegni@gmail.com>
2017-01-12 22:01:17 -0500

    almost there

--- EtC/Roulette.java
+++ EtC/Roulette.java
@@ -2,24 +2,125 @@ import cs1.Keyboard;
 
 public class Roulette extends Minigames{
 
+    private Driver player;
+    private int betOn;
+    private int winType;
+    
     public Roulette(){
 	betAmount = 0;
-	multiplier = 1;//to be Driver.getMoney() / constant
+	multiplier = 1;
+	player = new Driver();
+    }
+    
+    public Roulette(Driver playman){
+	betAmount = 0;
+	multiplier = 1;
+	player = playman;
     }
 
     public void play(){
-	
+
+	while (true){
+	    if (player.getMoney() <= 0){
+		System.out.println("You've run out of money!");
+	    }
+	    
+	    String s = "Welcome to Roulette!\n";
+	    s += "1. Play Game\n";
+	    s += "2. Go Back\n";
+	    System.out.print(s);
+	    
+	    int response = Keyboard.readInt();
+
+	    if (response == 2) return;
+	    
+	    if (response == 1){
+	        s = "Place your bet!\n";
+		System.out.print(s);
+		bet();
+
+		spin();
+		
+		int winAmount = winCondition();
+		player.alterMoney(winAmount);
+		System.out.println("You won $" + winCondition() + "!");
+	    }
+	}
     }
 
     private void spin(){
 	int result = (int)Math.random() * 41;
+	System.out.println("Enter a number 1-39, 41 for all Reds, 42 for all Blacks, or 0 for white.");
+	betOn = Keyboard.readInt();
+	System.out.println("The dealer spins the roulette, twisting his mustache as it slows down.");
+	if(betOn == 0 && (result == 0 || result == 40)){
+	    winType = 3;
+	    System.out.println("You Win!");
+	    return;
+	}
+	else{
+	    if(result == betOn){
+		winType = 2;
+		System.out.println("You Win!");
+	    }
+	    if(result % 2 == 0 && betOn == 42){
+		winType = 1;
+		System.out.println("You Win!");
+	    }
+	    if(result % 2 == 1 && betOn == 41){
+		winType = 1;
+		System.out.println("You Win!");
+	    }
+	    else{
+		System.out.println("Sorry, you lose. Try again.");
+	    }
+	}
     }
 
-    private void RPGBattle(){
+
+    private void bet(){
+        betAmount = Keyboard.readInt();
 	
+	if (betAmount > player.getMoney()){
+	    System.out.println("Amount greater than what you have on hand. Try again:");
+	    bet();
+	}
+	
+	else{
+	    player.alterMoney(betAmount * -1);
+	    System.out.println("Bet placed. Your money is now " + player.getMoney() + ".");
+	}
+    }
+
+    private int winCondition(){
+	if(winType == 1){
+	    return 2;
+	}
+	if(winType == 2){
+	    return 40;
+	}
+	if(winType == 3){
+	    return 20;
+	}
+	else{
+	    return 0;
+	}
+    }
+	    
+    
+    private void RPGBattle(){
+	System.out.println("Would you like to attack the dealer?");
+	//not yet implemented
     }
 
     private void eatBurger(){
+	//not yet implemented
+    }
+
+    public static void main(String[] args){
+	Roulette ashbob = new Roulette();
 	
+
+	ashbob.play();
     }
 }
