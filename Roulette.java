c870b169e67bb5ae817217e918c5ffe1d660ff13
Alessandro Cartegni <acartegni@stuy.edu>
2017-01-20 09:08:33 -0500

    battles

--- Roulette.java
+++ Roulette.java
@@ -28,6 +28,7 @@ public class Roulette extends Minigames{
 	    String s = "Welcome to Roulette!\n";
 	    s += "1. Play Game\n";
 	    s += "2. Go Back\n";
+	    s += "3. Fight the dealer\n";
 	    System.out.print(s);
 	    
 	    int response = Keyboard.readInt();
@@ -45,6 +46,12 @@ public class Roulette extends Minigames{
 		int winAmount = betAmount * (int)multiplier;
 		player.alterMoney(winAmount);
 		System.out.println("You won $" + winAmount + "!");
+		
+	    }
+
+	    if (response == 3){
+		RPGBattle();
+		return;
 	    }
 	}
     }
@@ -111,16 +118,40 @@ public class Roulette extends Minigames{
 	    
     
     private void RPGBattle(){
-	System.out.println("Would you like to attack the dealer?");
-	//not yet implemented
+	 String s = "Do you want to fight the dealer?\n";
+	    s += "1. LEEEROOOOOOY JENKIINS!!!\n";
+	    s += "2. Maybe later\n";
+	    System.out.print(s);
+	    
+	    int response = Keyboard.readInt();
+
+	    if (response == 2) {
+		System.out.println("You walk away in shame.");
+	    }
+	    
+	    if (response == 1){
+		System.out.println("You load your gun with chips and start shooting at the dealer.");
+		
+		if(player.getMoney() > 150){
+		    player.rouletteKill = true;
+		    String msgW = "";
+		    msgW += "Your shots collide at quasi-relativistic speeds (5 m/s) with the dealer.\n";
+		    msgW += "He falls to the floor, unconscious, and you leave victorious.";
+		    System.out.println(msg);
+		}
+		else{
+		    String msgL = "";
+		    msgL += "You realize too late that you do not have enough chips in the gun to do anything useful.\n";
+		    msgL += "The dealer throws a roulette ball at you and you walk away in shame.";
+		    System.out.println(msgL);
+		}
+	    }
+	
     }
 
     private void eatBurger(){
 	//not yet implemented
     }
 
-    /* public static void main(String[] args){
-	Roulette ashbob = new Roulette();
-	ashbob.play();
-	}*/
+    
 }
