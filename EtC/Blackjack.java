7a6ce17bc3070206e81cef3c78af7f2e3b23ec3e
Alessandro Cartegni <acartegni@gmail.com>
2017-01-21 15:22:35 -0500

    fight

--- EtC/Blackjack.java
+++ EtC/Blackjack.java
@@ -44,31 +44,37 @@ public class Blackjack extends Minigames{
 
 	    String s = "Welcome to Blackjack!\n";
 	    s += "1. Play Game\n";
-	    s += "2. Go Back\n";
+	    s += "2. Eat Burger\n";
+	    s += "3. Fight dealer\n";
+	    s += "4. Go back\n";
 	    System.out.print(s);
-
+	    
 	    int response = Keyboard.readInt();
-
-	    if (response == 2) return;
-
-	    if (response == 1) {
-		s = "Place your bet!\n";
+	    
+	    if (response == 4) return;
+	    
+	    if (response == 2){
+		eatBurger();
+	    }
+	    
+	    if (response == 1){
+	        s = "Place your bet!\n";
 		System.out.print(s);
 		bet();
-
+		
 		String b = "The dealer shuffles the deck. He hands it to you to cut.\n";
 		b += "1. Cut the deck\n";
 		b += "2. Take the cards and use it as a weapon against the dealer.\n";
 		System.out.print(s);
-
+		
 		int response1 = Keyboard.readInt();
-
+		
 		if (response1 == 2) {
 		    b = "Just kidding. That's probably a bad idea.\n";
 		    System.out.print(s);
 		    return;
 		}
-
+		
 		if (response1 == 1) {
 		    b = "You cut the deck, and the plastic insert card is placed.\n";
 		    System.out.print(b);
@@ -90,6 +96,11 @@ public class Blackjack extends Minigames{
 			}
 		    }
 		}
+		
+		if (response == 3){
+		    RPGBattle();
+		    return;
+		}
 	    }
 	}
     }
@@ -106,23 +117,63 @@ public class Blackjack extends Minigames{
     }
 
     private void readComb() {
-	for (i : combination) {
+	int valueOf = 0;
+	for (int i : combination) {
 	    if (i.substring(0,1) > 0 && (i.substring(0,1) < 10))
-		int valueOf = i.substring(0,1);
-	    if (i == "King"
-       private void bet(){
+		valueOf = i.substring(0,1);
+	    if (i == "King"){}
+	}
+    }
+
+    private void bet(){
         int holder = Keyboard.readInt();
 	
 	if (holder > player.getMoney()){
 	    System.out.println("Amount greater than what you have on hand. Try again:");
 	    bet();
 	}
-
 	else{
 	    betAmount = holder;
 	    player.alterMoney(betAmount * -1);
 	    System.out.println("Bet placed. Your money is now " + player.getMoney() + ".");
 	}
     }
-   
+
+    private void RPGBattle(){
+	String s = "Do you want to fight the dealer?\n";
+	s += "1. LEEEROOOOOOY JENKIINS!!!\n";
+	s += "2. Maybe later\n";
+	System.out.print(s);
+	
+	int response = Keyboard.readInt();
+	
+	if (response == 2) {
+	    System.out.println("You walk away in shame.");
+	}
+	
+	if (response == 1){
+	    System.out.println("You load your gun with chips and start shooting at the dealer.");
+	    
+	    if(player.getMoney() > 150){
+		player.rouletteKill = true;
+		String msgW = "";
+		msgW += "Your shots collide at quasi-relativistic speeds (5 m/s) with the dealer.\n";
+		msgW += "He falls to the floor, unconscious, and you leave victorious.";
+		System.out.println(msgW);
+	    }
+	    else{
+		String msgL = "";
+		msgL += "You realize too late that you do not have enough chips in the gun to do anything useful.\n";
+		msgL += "The dealer throws a card at you and you walk away in shame.";
+		System.out.println(msgL);
+	    }
+	}
+	
+    }
+
+    private void eatBurger(){
+	//not yet implemented
+    }
+    
+	
 }
